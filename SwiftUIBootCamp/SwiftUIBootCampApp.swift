de68524a0714eb558bdb897f128e93d3a37179cb
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2023-09-12 18:28:41 +0545

    Initial Commit

---
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Shishir Rijal on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
